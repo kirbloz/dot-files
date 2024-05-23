74eb0097826bc8462b8588d419c787d0cf49e264
kirbloz <one.baisini@gmail.com>
2024-05-23 15:17:45 +0200

    update 23-05-2024

--- .config/i3/scripts/power.sh
+++ .config/i3/scripts/power.sh
@@ -4,7 +4,8 @@ chosen=$(printf "Log Out\nSuspend\nRestart\nPower OFF" | rofi -dmenu -i -theme-s
 
 case "$chosen" in
 	"Log Out") i3-msg exit ;;
-	"Suspend") i3lock -i "/home/giovanni/Images/Lockscreen/lockscreen.png" && systemctl suspend ;;
+	"Suspend") bash ~/.config/i3/scripts/suspend.sh ;;
+		# i3lock -i "/home/giovanni/Images/Lockscreen/lockscreen.png" && systemctl suspend ;;
 	"Restart") systemctl reboot ;;
 	"Power OFF") systemctl poweroff ;;
 	*) exit 1 ;;
---
+++ .config/i3/scripts/suspend.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+i3lock -i "/home/giovanni/Images/Lockscreen/lockscreen.png" && systemctl suspend
